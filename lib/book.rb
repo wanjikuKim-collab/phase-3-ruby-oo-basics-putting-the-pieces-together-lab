fd5fb20a93653b0149eec886b3b4f0480ec13eec
Wanjiku Kimani <wanjikukimani24@gmail.com>
2023-02-08 16:04:17 +0300

    completed lab

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,26 @@
+class Book
+    attr_reader :title
+    attr_accessor :author, :genre, :page_count
+    attr_writer :turn_page
+
+    def initialize(title)
+        @title= title
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
+
+book = Book.new("And Then There Were None")
+book.title
+book.author = "Agatha Christie"
+book.page_count = 272
+book.genre = "Mystery"
+
+book =  Book.new("The World According to Garp")
+book.turn_page
+
 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,28 @@
 # Make your shoe class here!
+class Shoe
+    attr_reader :brand
+    attr_accessor :color, :size, :material , :condition
+    attr_writer :cobble
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+    end
+end
+
+
+shoe = Shoe.new("Adidas")
+shoe.brand
+
+shoe.color = "red"
+shoe.size = 9.5
+shoe.material = "suede"
+shoe.condition = "tattered"
+
+shoe = Shoe.new("Nike")
+shoe.cobble
+shoe.condition
